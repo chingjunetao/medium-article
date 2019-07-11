d540f389aa8837e8fb53f3bac1efb5a13de5eb8f
June Tao Ching <chingjunetao@gmail.com>
2019-07-11 22:31:06 +0800

    Removed unused code

--- google-search-console-api/google-search-console-data-crawler.py
+++ google-search-console-api/google-search-console-data-crawler.py
@@ -4,12 +4,11 @@ Created on Wed Mar  6 16:57:17 2019
 """
 
 import pickle
-import pandas as pd 
+import pandas as pd
 
 from datetime import datetime, timedelta
 from google_auth_oauthlib.flow import InstalledAppFlow
 from apiclient.discovery import build
-from google.oauth2 import service_account
 
 SITE_URL = "https://www.yourwebsite.com/"
 
@@ -22,7 +21,7 @@ OAUTH_SCOPE = ('https://www.googleapis.com/auth/webmasters.readonly', 'https://w
 # Redirect URI for installed apps
 REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
 
- 
+
 # This is auth flow walks you through the Web auth flow the first time you run the script and stores the credentials in a file
 # Every subsequent time you run the script, the script will use the "pickled" credentials stored in config/credentials.pickle
 try:
@@ -31,17 +30,15 @@ except (OSError, IOError) as e:
     flow = InstalledAppFlow.from_client_secrets_file("client-id.json", scopes=OAUTH_SCOPE)
     credentials = flow.run_console()
     pickle.dump(credentials, open("config/credentials.pickle", "wb"))
- 
-# Connect to Search Console Service using the credentials 
+
+# Connect to Search Console Service using the credentials
 webmasters_service = build('webmasters', 'v3', credentials=credentials)
 
 maxRows = 25000
 i = 0
 output_rows = []
-start_date = "2019-01-01"
-end_date = "2019-01-02"
-start_date = datetime.strptime(start_date, "%Y-%m-%d")
-end_date = datetime.strptime(end_date, "%Y-%m-%d")
+start_date = datetime.strptime("2019-01-01", "%Y-%m-%d")
+end_date = datetime.strptime("2019-01-02", "%Y-%m-%d")
 
 
 def date_range(start_date, end_date, delta=timedelta(days=1)):
@@ -60,13 +57,12 @@ def date_range(start_date, end_date, delta=timedelta(days=1)):
         current_date += delta
 
 
-
 for date in date_range(start_date, end_date):
     date = date.strftime("%Y-%m-%d")
     print(date)
     i = 0
     while True:
-        
+
         request = {
             'startDate' : date,
             'endDate' : date,
@@ -74,10 +70,10 @@ for date in date_range(start_date, end_date):
             "searchType": "Web",
             'rowLimit' : maxRows,
             'startRow' : i * maxRows
-        
+
         }
-        
-                    
+
+
         response = webmasters_service.searchanalytics().query(siteUrl = SITE_URL, body=request).execute()
         print()
         if response is None:
@@ -95,8 +91,7 @@ for date in date_range(start_date, end_date):
                 output_row = [date, keyword, page, country, device, row['clicks'], row['impressions'], row['ctr'], row['position']]
                 output_rows.append(output_row)
             i = i + 1
-        
+
 
 df = pd.DataFrame(output_rows, columns=['date','query','page', 'country', 'device', 'clicks', 'impressions', 'ctr', 'avg_position'])
 df.to_csv("gsc_output.csv")
-        
